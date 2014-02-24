a509cb00b51b6590ee3f1c5dc4671201fb578f7a
Sam Tubbax <sam@samtubbax.be>
2014-02-24 17:03:00 +0100

    Fix typo

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -28,7 +28,7 @@ $app->redirectIfTrailingSlash();
 
 $env = $app->detectEnvironment(array(
     'local' => array('Robot-Shmobot.local'),
-    'staging' => array('Mothership'),
+    'staging' => array('Mothership')
 ));
 
 /*
